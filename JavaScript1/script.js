47c3faff6fcfbe7bdc9d2cc16cdc3bdec5ab7acb
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-01-05 10:15:44 +0530

    Revising The basic of JavaScript

---
+++ JavaScript1/script.js
@@ -0,0 +1,14 @@
+alert(" Hey I am a Alert")
+
+var a = prompt(" Enter any number you want !!")
+console.log(a)
+var b = confirm(" are you sure ??")
+
+if (b){
+    console.log("Your PC is getting close")
+}
+else {
+    console.log("You are continue the working ")
+}
+
+document.body.style.backgroundColor = " Black"
